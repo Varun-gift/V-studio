fc2a8fd09ba3a9de12529cdb1dbad4c9b2fa2664
Forthe Nfl <nflforthe@gmail.com>
2025-07-28 17:30:48 +0000

    here is aa;;l my foorebase requirements  i ahve ,ade all the set uop rea

---
+++ src/lib/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTco3Kyvkf8hodV12sJMqyQ2jC8i5tcPk",
+  authDomain: "vstudio-3a136.firebaseapp.com",
+  projectId: "vstudio-3a136",
+  storageBucket: "vstudio-3a136.firebasestorage.app",
+  messagingSenderId: "578102210639",
+  appId: "1:578102210639:web:21d84a239697b90f1e7a3a",
+  measurementId: "G-VC15QQRW9Z"
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, db, storage };
